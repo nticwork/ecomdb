b1175fe109e32790ccca801824ed7212762d7b19
Azeggouar <k.azeggouar@gmail.com>
2026-01-19 21:44:52 +0100

    test3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,8 +19,7 @@
 <tr>
     <td>{{$item->nom  }}</td>
     <td>{{$item['prix']  }}DH</td>
-    <td>image : <img src="{{ asset('imgs/'.$item['image']) }}" alt="Image " class="img-fluid" width="100"></td>
-  </tr>
+    </tr>
 
 @endforeach
 
